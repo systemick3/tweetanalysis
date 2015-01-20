9299b42830ef9c13567b6936ab61f2f3c1d1b4ed
Mike Garthwaite <michael@michael.(none)>
2015-01-20 20:24:25 +0000

    Moved all controllers to home controller file

--- app/components/home/homeControllers.js
+++ app/components/home/homeControllers.js
@@ -1,5 +1,53 @@
 var app = angular.module('twitterapp');
 
+app.controller('homeCtrl', ['$scope', '$window', '$rootScope', 'ipCookie', 'userFactory', 'tConfig', function ($scope, $window, $rootScope, ipCookie, userFactory, tConfig) {
+
+  var userId;
+
+  // If the user refreshes a page retrieve the token from sessionStorage
+  if (angular.isDefined($window.sessionStorage.token) && (!angular.isDefined($rootScope.tweetapp) || $rootScope.tweetapp.authorised == false)) {
+    $rootScope.tweetapp = {};
+    $rootScope.tweetapp.authorised = true;
+    ipCookie(tConfig.sessionCookieName, $window.sessionStorage.token, { expires:365 });
+  }
+  // If sessionStorage isn't available try the cookie
+  else {
+    token = ipCookie(tConfig.sessionCookieName);
+    if (angular.isDefined(token) && (!angular.isDefined($rootScope.tweetapp) || $rootScope.tweetapp.authorised == false)) {
+      $window.sessionStorage.token = token;
+      $rootScope.tweetapp = {};
+      $rootScope.tweetapp.authorised = true;
+    }
+  }
+
+  // Fetch the session data from the API
+  if (angular.isDefined($rootScope.tweetapp) && $rootScope.tweetapp.authorised) {
+
+    userFactory.userSessionData().then(function (data) {
+
+      $window.sessionStorage.user_id = data.data.user_id;
+      $window.sessionStorage.screen_name = data.data.screen_name;
+      $scope.user = data.data;
+      $scope.tweetapp.user = data.data;
+      userId = data.data.user_id;
+      $rootScope.user = $scope.user;
+      $scope.tweets_for = 'user ' + $scope.user.screen_name;
+
+      // Get the full user data from Twitter
+      userFactory.userTwitterData(data.data.user_id)
+        .success(function (data) {
+          var mongo_id = $scope.user._id;
+          $scope.user = data;
+          $scope.user._id = mongo_id;
+          console.log($scope.user);
+        });
+
+    });
+
+  };
+
+}]);
+
 app.controller('streamCtrl', ['$scope', 'socket', 'homeFactory', function ($scope, socket, homeFactory) {
   var oldTweets = [];
   $scope.streamtweets = [];
@@ -12,7 +60,7 @@ app.controller('streamCtrl', ['$scope', 'socket', 'homeFactory', function ($scop
     else {
       oldTweets = $scope.streamtweets;
     }
-    $scope.streamtweets = processTweets(data.concat(oldTweets));
+    $scope.streamtweets = homeFactory.processTweets(data.concat(oldTweets));
   });
 
   homeFactory.getTrends()
@@ -47,4 +95,196 @@ app.controller('userAnalysisCtrl', ['$scope', 'userFactory', 'homeFactory', func
       });
     });
   });
+}]);
+
+app.controller('userTweetsCtrl', ['$scope', 'userFactory', 'homeFactory', function ($scope, userFactory, homeFactory) {
+  var userId;
+  userFactory.userSessionData().then(function (data) {
+    $scope.usertweets = null;
+    $scope.tweetsLoaded = false;
+    $scope.allTweetsLoaded = false;
+    homeFactory.getUserTweets($scope.user)
+      .success(function (data) {
+        $scope.tweetsLoaded = true;
+        $scope.usertweets = homeFactory.processTweets(data);
+      })
+      .error(function (error) {
+        $scope.status = 'Unable to load tweets for user: ' + error.message;
+      });
+
+    ////////////////// Functions used by the home page /////////////////////////
+
+    // Add more tweets to the end of the list
+    $scope.getMoreTweets = function () {
+      $scope.tweetsLoaded = false;
+      var maxId = $scope.usertweets[$scope.usertweets.length - 1].id_str;
+      homeFactory.getUserTweets($scope.user, maxId)
+        .success(function (data) {
+          var newTweets = homeFactory.processTweets(data.slice(1));
+          $scope.usertweets = $scope.usertweets.concat(newTweets);
+          $scope.tweetsLoaded = true;
+
+          if (newTweets.length < tConfig.numUserTweets) {
+            $scope.allTweetsLoaded = true;
+          }
+        });
+    };
+
+    // Show all the retweeters of a tweet
+    $scope.showRetweeters = function (tweetId) {
+      var selectedTweet;
+      for (var i=0; i<$scope.usertweets.length; i++) {
+        if ($scope.usertweets[i].id_str == tweetId) {
+          selectedTweet = $scope.usertweets[i];
+          break;
+        }
+      }
+
+      // If already loaded just show
+      if (selectedTweet.retweeters) {
+        selectedTweet.show_retweeters = true;
+      }
+      // Fetch from server
+      else {
+
+        homeFactory.getRetweeters(tweetId)
+          .success(function (data) {
+            selectedTweet.retweeters = data.retweeters;
+            selectedTweet.reach = data.reach;
+            selectedTweet.show_retweeters = true;
+          })
+          .error(function (err) {
+            console.log(err);
+          });
+      }
+
+    };
+
+    // Hide the retweeters info
+    $scope.hideRetweeters = function (tweetId) {
+      for (var i=0; i<$scope.usertweets.length; i++) {
+        if ($scope.usertweets[i].id_str == tweetId) {
+          $scope.usertweets[i].show_retweeters = false;
+          break;
+        }
+      }
+    };
+
+    // Show the replies to this tweets
+    $scope.showReplies = function(tweetId) {
+      var selectedTweet;
+
+      for (var i=0; i<$scope.usertweets.length; i++) {
+        if ($scope.usertweets[i].id_str == tweetId) {
+          selectedTweet = $scope.usertweets[i];
+          break;
+        }
+      }
+      if (selectedTweet.replies) {
+        selectedTweet.show_replies = true;
+      }
+      else {
+
+        homeFactory.getReplies(userId, tweetId)
+          .success(function (data) {
+            selectedTweet.replies = homeFactory.processTweets(data.replies);
+            selectedTweet.show_replies = true;
+          })
+          .error(function (err) {
+            console.log(err);
+          });
+      }
+
+    };
+
+    // Hide the replies
+    $scope.hideReplies = function (tweetId) {
+      for (var i=0; i<$scope.usertweets.length; i++) {
+        if ($scope.usertweets[i].id_str == tweetId) {
+          $scope.usertweets[i].show_replies = false;
+          break;
+        }
+      }
+    };
+
+    // Show the sentiment for this tweets
+    $scope.showSentiment = function (tweetId, replyId) {
+      var selectedTweet,
+        replies;
+
+      // Find the tweet that has been selected
+      for (var i=0; i<$scope.usertweets.length; i++) {
+        if ($scope.usertweets[i].id_str == tweetId) {
+          selectedTweet = $scope.usertweets[i];
+          break;
+        }
+      }
+
+      // If the tweet is a reply find it in the replies
+      // of the selected tweet
+      if (replyId) {
+        replies = selectedTweet.replies;
+        for (var j=0; j < replies.length; j++) {
+          if (replies[j].id_str == replyId) {
+            selectedTweet = replies[j];
+          }
+        }
+      }
+
+      // If sentiment has been fetched from server just show it
+      if (selectedTweet.sentiment) {
+        selectedTweet.show_sentiment = true;
+      }
+      else {
+        // Fetch from server
+        homeFactory.getSentiment(selectedTweet.id_str, replyId)
+          .success(function (data) {
+            selectedTweet.sentiment = data.sentiment;
+            selectedTweet.show_sentiment = true;
+          })
+          .error(function (err) {
+            console.log(err);
+          });
+
+      }
+    };
+
+    // Hide the sentiment
+    $scope.hideSentiment = function (tweetId, replyId) {
+      var selectedTweet,
+        replies;
+
+      // Find the tweet that has been selected
+      for (var i=0; i<$scope.usertweets.length; i++) {
+        if ($scope.usertweets[i].id_str == tweetId) {
+          selectedTweet = $scope.usertweets[i];
+          break;
+        }
+      }
+
+      // If this is a reply then find it in the replies
+      // of the selected tweet
+      if (replyId) {
+        replies = selectedTweet.replies;
+        for (var j=0; j<replies.length; j++) {
+          if (replies[j].id_str == replyId) {
+            selectedTweet = replies[j];
+          }
+        }
+      }
+
+      selectedTweet.show_sentiment = false;
+    };
+
+  });
+
+}]);
+
+app.controller('errorCtrl', ['$scope', '$window', '$location', function($scope, $window, $location) {
+    
+}]);
+
+// default - handle any requests not for an authorised URL
+app.controller('defaultCtrl', ['$scope', '$window', '$location', function($scope, $window, $location) {
+  $location.path('/home');
 }]);
--- app/components/home/homeServices.js
+++ app/components/home/homeServices.js
@@ -73,6 +73,24 @@ app.factory("homeFactory", ['$http', 'tConfig', function ($http, tConfig) {
     getTrends: function () {
       var trendsUrl = apiData.server + apiData.trends.url;
       return $http.get(trendsUrl);
+    },
+
+    processTweets: function (tweets) {
+      for (var i=0; i<tweets.length; i++) {
+        tweets[i].display_text = this.processTweetLinks(tweets[i].text);
+        tweets[i].short_date = tweets[i].created_at.substring(0, 16);
+      }
+      return tweets;
+    },
+
+    processTweetLinks: function (text) {
+      var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+      text = text.replace(exp, "<a href='$1' target='_blank'>$1</a>");
+      exp = /(^|\s)#(\w+)/g;
+      text = text.replace(exp, "$1<a href='http://search.twitter.com/search?q=%23$2' target='_blank'>#$2</a>");
+      exp = /(^|\s)@(\w+)/g;
+      text = text.replace(exp, "$1<a href='http://www.twitter.com/$2' target='_blank'>@$2</a>");
+      return text;
     }
 
   };
