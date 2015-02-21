17a39865b422dc39ce880a5e04b936dd1f003f4a
Mike Garthwaite <michael@michael.(none)>
2015-02-21 17:47:25 +0000

    Removed alert

--- app/components/about/aboutControllers.js
+++ app/components/about/aboutControllers.js
@@ -5,7 +5,6 @@ app.controller('aboutCtrl', ['$scope', '$window', function ($scope, $window) {
   $scope.showAbout = false;
 
   $scope.toggleAbout = function() {
-    alert('Hello');
     $window.scrollTo(0, 0);
     $scope.showAbout = !$scope.showAbout;
   };
