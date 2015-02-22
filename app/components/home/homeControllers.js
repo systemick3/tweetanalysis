3e7b782dc1d48054bbae7af6b074c9e81963194c
Mike Garthwaite <michael@michael.(none)>
2015-02-22 13:14:11 +0000

    Moved tweets into component

--- app/components/home/homeControllers.js
+++ app/components/home/homeControllers.js
@@ -1,6 +1,6 @@
 var app = angular.module('twitterapp');
 
-app.controller('homeCtrl', ['$scope', '$window', '$rootScope', 'ipCookie', 'userFactory', 'homeFactory', 'tConfig', function ($scope, $window, $rootScope, ipCookie, userFactory, homeFactory, tConfig) {
+app.controller('homeCtrl', ['$scope', '$window', '$rootScope', 'ipCookie', 'userFactory', 'tConfig', function ($scope, $window, $rootScope, ipCookie, userFactory, tConfig) {
 
   var userId;
 
@@ -58,81 +58,41 @@ app.controller('homeCtrl', ['$scope', '$window', '$rootScope', 'ipCookie', 'user
 
 }]);
 
-app.controller('streamCtrl', ['$scope', 'socket', 'homeFactory', function ($scope, socket, homeFactory) {
-  var oldTweets = [];
-  $scope.streamtweets = [];
+// app.controller('streamCtrl', ['$scope', 'socket', 'homeFactory', function ($scope, socket, homeFactory) {
+//   var oldTweets = [];
+//   $scope.streamtweets = [];
 
-  socket.on('tweets', function (data) {
-    // Display a maximum of 12 tweets
-    if ($scope.streamtweets.length >= 12) {
-      // If we already have 12 tweets lose the oldest
-      $scope.streamtweets.pop();
-      oldTweets = $scope.streamtweets;
-    }
-    else {
-      oldTweets = $scope.streamtweets;
-    }
-    // Make the new tweet the 1st item in the array
-    $scope.streamtweets = homeFactory.processTweets(data.concat(oldTweets));
-  });
+//   socket.on('tweets', function (data) {
+//     // Display a maximum of 12 tweets
+//     if ($scope.streamtweets.length >= 12) {
+//       // If we already have 12 tweets lose the oldest
+//       $scope.streamtweets.pop();
+//       oldTweets = $scope.streamtweets;
+//     }
+//     else {
+//       oldTweets = $scope.streamtweets;
+//     }
+//     // Make the new tweet the 1st item in the array
+//     $scope.streamtweets = homeFactory.processTweets(data.concat(oldTweets));
+//   });
 
-  $scope.$on('socket:error', function (ev, data) {
-    $scope.streamError = 'Unable to stream latest tweets from Twitter. Please try again later.'
-  });
+//   $scope.$on('socket:error', function (ev, data) {
+//     $scope.streamError = 'Unable to stream latest tweets from Twitter. Please try again later.'
+//   });
 
-  homeFactory.getTrends()
-    .success(function (data) {
-      $scope.trends = data.data[0].trends;
-    })
-    .error(function (err) {
-      $scope.trendsError = 'Unable to get latest trends from Twitter. Please try again later.';
-    });
+//   homeFactory.getTrends()
+//     .success(function (data) {
+//       $scope.trends = data.data[0].trends;
+//     })
+//     .error(function (err) {
+//       $scope.trendsError = 'Unable to get latest trends from Twitter. Please try again later.';
+//     });
 
-  $scope.selectTrends = function (trend) {
-    return trend.name.length <= 30;
-  };
+//   $scope.selectTrends = function (trend) {
+//     return trend.name.length <= 30;
+//   };
 
-}]);
-
-app.controller('userTweetsCtrl', ['$scope', 'userFactory', 'homeFactory', 'tConfig', function ($scope, userFactory, homeFactory, tConfig) {
-  var userId;
-  userFactory.userSessionData().then(function (data) {
-    $scope.usertweets = null;
-    $scope.tweetsLoaded = false;
-    $scope.allTweetsLoaded = false;
-    homeFactory.getUserTweets($scope.user)
-      .success(function (data) {
-        $scope.tweetsLoaded = true;
-        $scope.usertweets = homeFactory.processTweets(data);
-      })
-      .error(function (error) {
-        $scope.status = 'Unable to load tweets for user: ' + error.message;
-      });
-
-    ////////////////// Functions used by the home page /////////////////////////
-
-    // Add more tweets to the end of the list
-    $scope.getMoreTweets = function () {
-      $scope.tweetsLoaded = false;
-      var maxId = $scope.usertweets[$scope.usertweets.length - 1].id_str;
-      homeFactory.getUserTweets($scope.user, maxId)
-        .success(function (data) {
-          var newTweets = homeFactory.processTweets(data.slice(1));
-          $scope.usertweets = $scope.usertweets.concat(newTweets);
-          $scope.tweetsLoaded = true;
-
-          if (newTweets.length < tConfig.numUserTweets) {
-            $scope.allTweetsLoaded = true;
-          }
-        })
-        .error(function (err) {
-          $scope.moreTweetsError = 'Unable to download more tweets. Please try again later.';
-        });
-    };
-
-  });
-
-}]);
+// }]);
 
 app.controller('headerCtrl', ['$scope', '$window', '$location', function($scope, $window, $location) {
 
---
+++ app/components/tweet/tweetServices.js
@@ -0,0 +1,81 @@
+var app = angular.module("twitterapp");
+
+app.factory("tweetFactory", ['$http', 'tConfig', function ($http, tConfig) {
+  var TWEETS_PER_REQUEST = 5,
+    userMentionsPromise,
+    apiData = tConfig.apiData;
+
+  return {
+
+    getUserTweets: function (user, maxId) {
+      var userTweetsUrl = apiData.server + apiData.userTweets.url + '/' + user.screen_name + '/' + tConfig.numUserTweets;
+
+      if (maxId) {
+        userTweetsUrl += '/' + maxId;
+      }
+
+      return $http.get(userTweetsUrl);
+    },
+
+    getTweet: function(id) {
+      var oneTweetUrl = apiData.server + apiData.oneTweet.url + '/' + id;
+      return $http.get(oneTweetUrl);
+    },
+
+    getRetweeters: function(tweetId) {
+      var retweetersUrl = apiData.server + apiData.retweeters.url + '/' + tweetId;
+      return $http.get(retweetersUrl);
+    },
+
+    getUserMentions: function (userId) {
+      var mentionsUrl = apiData.server + apiData.mentions.url + '/' + userId;
+
+      if (!userMentionsPromise) {
+        userMentionsPromise = $http.get(mentionsUrl).then(function (response) {
+          return response;
+        });
+      }
+      return userMentionsPromise;
+    },
+
+    getReplies: function (userId, tweetId) {
+      var repliesUrl = apiData.server + apiData.replies.url + '/' + userId + '/' + tweetId;
+      return $http.get(repliesUrl);
+    },
+
+    getSentiment: function (tweetId, isReply) {
+      var sentimentUrl = apiData.server + apiData.sentiment.url + '/' + tweetId;
+
+      if (isReply) {
+        sentimentUrl += '/' + 'true';
+      }
+
+      return $http.get(sentimentUrl);
+    },
+
+    getTrends: function () {
+      var trendsUrl = apiData.server + apiData.trends.url;
+      return $http.get(trendsUrl);
+    },
+
+    processTweets: function (tweets) {
+      for (var i=0; i<tweets.length; i++) {
+        tweets[i].display_text = this.processTweetLinks(tweets[i].text);
+        tweets[i].short_date = tweets[i].created_at.substring(0, 16);
+      }
+      return tweets;
+    },
+
+    processTweetLinks: function (text) {
+      var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+      text = text.replace(exp, "<a href='$1' target='_blank'>$1</a>");
+      exp = /(^|\s)#(\w+)/g;
+      text = text.replace(exp, "$1<a href='http://search.twitter.com/search?q=%23$2' target='_blank'>#$2</a>");
+      exp = /(^|\s)@(\w+)/g;
+      text = text.replace(exp, "$1<a href='http://www.twitter.com/$2' target='_blank'>@$2</a>");
+      return text;
+    }
+
+  };
+
+}]);
